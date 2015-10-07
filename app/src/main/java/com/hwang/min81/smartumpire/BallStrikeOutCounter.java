f731eff3b96a1c13b30f521c6bb0b89d55a785d0
Min Hwang <min81.hwang@gmail.com>
2015-10-08 00:44:37 +0900

    Add BallStrikeOutCounter class and corresponding tests

--- app/src/main/java/com/hwang/min81/smartumpire/BallStrikeOutCounter.java
+++ app/src/main/java/com/hwang/min81/smartumpire/BallStrikeOutCounter.java
@@ -4,12 +4,25 @@ package com.hwang.min81.smartumpire;
  * Created by min81 on 2015-10-07.
  */
 public class BallStrikeOutCounter {
-    private short ball = 0;
 
-    public short getBall() { return this.ball; }
+    private short ball = 0, strike = 0;
+
+    public short getBall() {
+        return this.ball;
+    }
+
     public void setBall(short ball) throws OutOfRangeException {
         if(ball > 3) { throw new OutOfRangeException(); }
         this.ball = ball;
+    }
+
+    public short getStrike() {
+        return this.strike;
+    }
+
+    public void setStrike(short strike) throws OutOfRangeException {
+        if(strike > 2) { throw new OutOfRangeException(); }
+        this.strike = strike;
     }
 
     class OutOfRangeException extends Exception {
--- app/src/test/java/com/hwang/min81/smartumpire/BallStrikeOutCounterTest.java
+++ app/src/test/java/com/hwang/min81/smartumpire/BallStrikeOutCounterTest.java
@@ -14,6 +14,12 @@ public class BallStrikeOutCounterTest {
         assertEquals(0, ballStrikeOutCounter.getBall());
     }
 
+    @Test
+    public void Strike_CanDeliverTheDefaultValue_0() {
+        BallStrikeOutCounter ballStrikeOutCounter = new BallStrikeOutCounter();
+        assertEquals(0, ballStrikeOutCounter.getStrike());
+    }
+
     /*
      * setBall() 함수가 예외를 발생시키도록 수정되면서 함수 시그너처가 바뀌었음.
     @Test
@@ -34,6 +40,7 @@ public class BallStrikeOutCounterTest {
     }
     */
 
+    //// TODO: 2015-10-08 Parameterized test 고려해볼 필요 있음.
     @Test
     public void Ball_CanBeAssignedTheValue_0to3() throws BallStrikeOutCounter.OutOfRangeException {
         BallStrikeOutCounter ballStrikeOutCounter = new BallStrikeOutCounter();
@@ -51,12 +58,33 @@ public class BallStrikeOutCounterTest {
         assertEquals(3, ballStrikeOutCounter.getBall());
     }
 
+    @Test
+    public void Strike_CanBeAssignedTheValue_0to2() throws BallStrikeOutCounter.OutOfRangeException {
+        BallStrikeOutCounter ballStrikeOutCounter = new BallStrikeOutCounter();
+
+        ballStrikeOutCounter.setStrike((short) 0);
+        assertEquals(0, ballStrikeOutCounter.getStrike());
+
+        ballStrikeOutCounter.setStrike((short) 1);
+        assertEquals(1, ballStrikeOutCounter.getStrike());
+
+        ballStrikeOutCounter.setStrike((short) 2);
+        assertEquals(2, ballStrikeOutCounter.getStrike());
+    }
+
     // 아래 테스트는 2개의 입력값에 대한 테스트가 이루어지지 않음.
     // TODO 추후 다른 테스트 방법을 통해 개선되어야 함.
     @Test(expected = BallStrikeOutCounter.OutOfRangeException.class)
-    public void Ball_AssigningTheValueGreaterThan3_ThrowsOverflowException() throws BallStrikeOutCounter.OutOfRangeException {
+    public void Ball_AssigningTheValueGreaterThan3_ThrowsOutOfRangeException() throws BallStrikeOutCounter.OutOfRangeException {
         BallStrikeOutCounter ballStrikeOutCounter = new BallStrikeOutCounter();
-        ballStrikeOutCounter.setBall((short)4);
-        ballStrikeOutCounter.setBall((short)5);
+        ballStrikeOutCounter.setBall((short) 4);
+        ballStrikeOutCounter.setBall((short) 5);
+    }
+
+    @Test(expected = BallStrikeOutCounter.OutOfRangeException.class)
+    public void Strike_AssigningTheValueGreaterThan2_ThrowsOutOfRangeException() throws BallStrikeOutCounter.OutOfRangeException {
+        BallStrikeOutCounter ballStrikeOutCounter = new BallStrikeOutCounter();
+        ballStrikeOutCounter.setStrike((short) 3);
+        ballStrikeOutCounter.setStrike((short) 4);
     }
 }
