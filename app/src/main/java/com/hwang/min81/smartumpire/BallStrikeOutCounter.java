dbbb82b3ca663214cd4114934b7cad9c297a855c
Min Hwang <min81.hwang@gmail.com>
2015-10-13 01:32:28 +0900

    Add BallStrikeOutCounter class and corresponding tests

--- app/src/main/java/com/hwang/min81/smartumpire/BallStrikeOutCounter.java
+++ app/src/main/java/com/hwang/min81/smartumpire/BallStrikeOutCounter.java
@@ -5,7 +5,7 @@ package com.hwang.min81.smartumpire;
  */
 public class BallStrikeOutCounter {
 
-    private short ball = 0, strike = 0;
+    private short ball = 0, strike = 0, out = 0;
 
     public short getBall() {
         return this.ball;
@@ -23,6 +23,53 @@ public class BallStrikeOutCounter {
     public void setStrike(short strike) throws OutOfRangeException {
         if(strike > 2) { throw new OutOfRangeException(); }
         this.strike = strike;
+    }
+
+    public short getOut() {
+        return this.out;
+    }
+
+    public void setOut(short out) throws OutOfRangeException {
+        if(out > 2) { throw new OutOfRangeException(); }
+        this.out = out;
+    }
+
+    public void addOneStrike() throws OutOfRangeException {
+        final short strike = this.getStrike();
+
+        if(strike == 2) {
+            this.setStrike((short)0);
+            this.setBall((short) 0);
+            this.addOneOut();
+        }
+        else {
+            this.setStrike((short)(strike + 1));
+        }
+    }
+
+    public void addOneOut() throws OutOfRangeException {
+        final short out = this.getOut();
+
+        if(out == 2) {
+            this.setStrike((short)0);
+            this.setBall((short)0);
+            this.setOut((short) 0);
+        }
+        else {
+            this.setOut((short) (out + 1));
+        }
+    }
+
+    public void addOneBall() throws OutOfRangeException {
+        final short ball = this.getBall();
+
+        if(ball == 3) {
+            this.setStrike((short)0);
+            this.setBall((short)0);
+        }
+        else {
+            this.setBall((short) (ball + 1));
+        }
     }
 
     class OutOfRangeException extends Exception {
