08989bdc52cc02bcb5e51e3d7589404a5e15ad10
Min Hwang <min81.hwang@gmail.com>
2015-11-24 00:07:07 +0900

    Added Timer class and test

---
+++ app/src/test/java/com/hwang/min81/smartumpire/TimerTest.java
@@ -0,0 +1,22 @@
+package com.hwang.min81.smartumpire;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hwangmin on 2015. 11. 24..
+ */
+public class TimerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
