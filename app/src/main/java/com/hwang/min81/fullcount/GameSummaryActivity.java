5ed71888812b4b792ab2adccc867aed72b8da32f
min <min81.hwang@gmail.com>
2016-02-14 13:46:58 +0900

    Using score board fragment

--- app/src/main/java/com/hwang/min81/fullcount/GameSummaryActivity.java
+++ app/src/main/java/com/hwang/min81/fullcount/GameSummaryActivity.java
@@ -1,7 +1,10 @@
 package com.hwang.min81.fullcount;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class GameSummaryActivity extends AppCompatActivity {
 
